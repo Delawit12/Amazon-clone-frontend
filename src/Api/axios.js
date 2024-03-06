516536a20d3f8b8d54c48e611a69da8afa6a95cb
Delawit12 <delawitgetu@gmail.com>
2024-03-06 04:50:16 -0800

    axios update

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //     baseURL: "https://cute-ruby-cricket-gown.cyclic.cloud/"
-  //    // baseURL: "https://amazon-clone-g4tg.onrender.com"
-  baseURL: "http://localhost:7088/",
+  // deploy on cyclic
+  baseURL: "https://busy-gray-ox-shoe.cyclic.app/",
+  // deploy on render
+  // baseURL: "https://amazon-backend-fj8v.onrender.com"
+  // baseURL: "http://localhost:7088/",
 });
 
 export default axiosInstance;
