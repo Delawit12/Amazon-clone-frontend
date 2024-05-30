d38c6a1c3f4de44588a2b3c0c65c60b56df57eb2
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2024-05-30 14:38:39 +0300

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   // baseURL: "https://busy-gray-ox-shoe.cyclic.app/",
   // deploy on render
   // baseURL: "https://amazon-backend-fj8v.onrender.com"
-  baseURL: "https://amazon-backend-b9ni.onrender.com",
+  baseURL: "https://amazon-backend-xu4x.onrender.com",
   // baseURL: "http://localhost:7088/",
 });
 
